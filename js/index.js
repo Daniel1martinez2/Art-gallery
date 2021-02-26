3115d84138737d43dbac437b992b5286c39e2b77
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-02-25 22:49:49 -0500

    some issues with sizes in hero section
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const circleBig = document.querySelector('.hero__text-wrapper');
 const circleArt = circleBig.querySelectorAll('h1');
 circleArt.forEach((elem, i) => {
-  elem.style.top = `${95*i}px`;
+  elem.style.top = `${80*i}px`;
 })
