18027720f81f0d1ec95e0fbdc6eb4e40fe4342f3
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-02-26 10:30:43 -0500

    main room elements setting up yay inner architecture
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ const circleBig = document.querySelector('.hero__text-wrapper');
 const circleArt = circleBig.querySelectorAll('h1');
 circleArt.forEach((elem, i) => {
   elem.style.top = `${80*i}px`;
-})
+});
