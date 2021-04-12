1da33d95b8e7c6f0a1fc4191149c15ab4c99f5ad
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-04-12 09:35:54 -0500

    edit form working all rigth
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- js/editProductForm.js
+++ js/editProductForm.js
@@ -6,11 +6,14 @@ const filesArray = [];
 
 
 form.file.addEventListener('change', () => {
+  let file = form.file.files[0];
+  if (!file) return
   let reader = new FileReader();
   reader.onload = (e) => {
     imgThumb.style.backgroundImage = `url(${e.target.result})`;
   }
-  reader.readAsDataURL(form.file.files[0]); // convert to base64 string
+  // convert to base64 string
+  reader.readAsDataURL(form.file.files[0]);
   //add current selected file to the array
   filesArray.push(form.file.files[0]);
   console.log(filesArray);
@@ -18,11 +21,10 @@ form.file.addEventListener('change', () => {
   imgThumb.classList.add('edit-form__img-prev');
   imgThumb.innerHTML =
     `<button>
-    <img src="./lib/svg/close-salmon.svg" alt="">
-  </button>`;
+      <img src="./lib/svg/close-salmon.svg" alt="">
+    </button>`;
   prevContainer.appendChild(imgThumb);
 })
-
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const product = {
@@ -34,37 +36,67 @@ form.addEventListener('submit', (event) => {
     rating: parseFloat(form.rating.value),
     technique: form.technique.value,
     vanguard: form.vanguard.value,
-    description: form.description.value, 
+    description: form.description.value,
     images: [],
+  };
+  const genericCatch = (error)=> {
+    console.log('There was an error in the product upload');
   }
-  let storageRef = firebase.storage().ref();
-  filesArray.forEach((file)=>{
-    let fileRef = storageRef.child(`images/${file.name}`);
-    fileRef.put(file).then((snapshot) => {
-      snapshot.ref.getDownloadURL().then((downloadURL) => {
-        product.images.push({
-          imageUrl: downloadURL,
-          imageRef: snapshot.ref.fullPath,
-        }); 
-        console.log('File available at', downloadURL);
-        console.log(product);
-        //dudaaaaaa-------->>>>>>>---->>>>-----
-        if(product.images[2]!= undefined){
-          console.log('eoooooo');
-          db.collection("products").add(product)
-          .then((docRef) => {
-            console.log("Document added", docRef.id);
-          })
-          .catch((error) => {
-            console.error("Error adding document: ", error);
-          });
-        }
+  db.collection("products").add(product)
+    .then((docRef) => {
+      //reference to all promises i will do, so then i can wait for all to finish
+      const uploadPromises = [];
+      const downloadUrlPromises = [];
+      
+      filesArray.forEach( (file)=> {
+        let storageRef = firebase.storage().ref();
+        let fileRef = storageRef.child(`products/${docRef.id}/${file.name}`);
+        // espera a subir la imagen
+        uploadPromises.push(fileRef.put(file));
       });
-    });
+      //
+      Promise.all(uploadPromises).then( (snapshots)=> {
+        snapshots.forEach( (snapshot)=> {
+          // espera a obtener la url de descarga de la imagen
+          downloadUrlPromises.push(snapshot.ref.getDownloadURL());
+        });
+        Promise.all(downloadUrlPromises).then( (downloadURLs)=> {
   
-  }); 
-
-
-
-
+          const images = [];
+          downloadURLs.forEach((url, index) => {
+            images.push({
+              url: url,
+              ref: snapshots[index].ref.fullPath
+            });
+          });
+          db.collection('products').doc(docRef.id).update({
+            images: images
+          }).then( ()=>{
+              form.name.value = ''; 
+              form.author.value = ''; 
+              form.year.value = ''; 
+              form.price.value = ''; 
+              form.country.value = ''; 
+              form.rating.value = ''; 
+              form.technique.value = ''; 
+              form.vanguard.value = ''; 
+              form.description.value = ''; 
+              prevContainer.innerHTML = ''; 
+              //reset files array
+              while (filesArray.length) {
+                filesArray.pop();
+              }
+              console.log(filesArray);
+          })
+          .catch(genericCatch);
+        })
+        .catch(genericCatch);
+      })
+      .catch(genericCatch);
+      //
+      console.log("Document added", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
 });
